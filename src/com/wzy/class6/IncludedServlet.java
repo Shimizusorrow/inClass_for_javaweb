161860d19ce51779e4ca642f9e1bf2da32e347b6
Shimizusorrow <1047791704@qq.com>
2018-11-28 10:44:14 +0800

    change the class6

---
+++ src/com/wzy/class6/IncludedServlet.java
@@ -0,0 +1,26 @@
+package com.wzy.class6;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "IncludedServlet",urlPatterns = "/IncludedServlet")
+public class IncludedServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out=response.getWriter();
+        out.println("中国"+"<br>");
+        out.println("URL:"+request.getRequestURI()+"<br>");
+        out.println("QueryString"+request.getQueryString()+"<br>");
+        out.println("parameter pl:" +request.getParameter("pl")+"<br>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
---
+++ src/com/wzy/class6/RequestDispathcherIncludeServlet.java
@@ -0,0 +1,25 @@
+package com.wzy.class6;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "RequestDispathcherIncludeServlet",urlPatterns = "/RequestDispathcherIncludeServlet")
+public class RequestDispathcherIncludeServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out=response.getWriter();
+        RequestDispatcher  rd=request.getRequestDispatcher("/IncludedServlet?pl=abc");
+        out.println("before including"+"<br>");
+        rd.include(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
