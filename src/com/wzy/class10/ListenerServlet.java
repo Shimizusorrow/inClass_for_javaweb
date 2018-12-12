e96afaf8e562e24f37d2bb1003ad9a8be97f2215
Administrator <12313@qq.com>
2018-12-12 10:01:35 +0800

    添加了监听器

---
+++ src/com/wzy/class10/ListenerServlet.java
@@ -0,0 +1,24 @@
+package com.wzy.class10;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "ListenerServlet",urlPatterns = "/ListenerServlet")
+public class ListenerServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session=request.getSession();
+        session.setAttribute("username","jxx");
+//        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().println("session对象的域属性username创建成功");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
